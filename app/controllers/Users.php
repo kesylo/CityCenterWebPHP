f1b9754284e180b31d6d6555a58374952cc86802
kesylo <keming.loic@gmail.com>
2019-10-24 14:32:14 +0200

    Added validation on login fields

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -9,10 +9,39 @@ class Users extends Controller {
 
     }
 
-    public function login(){
-        // check if post
+    public function login(){    // check if post
+
         if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-            // process form
+            // make sure text field are clean
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            // init data
+            $data = [
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'email_err' => '',
+                'password_err' => '',
+            ];
+
+            // validate email
+            if (empty($data['email'])){
+                $data['email_err'] = 'Entrez une adresse email valide.';
+            }
+
+            // validate password
+            if (empty($data['password'])){
+                $data['password_err'] = 'Entrez un mot de  passe valide.';
+            }
+
+            // make sure errors are empty
+            if (empty($data['email_err']) && empty($data['password_err'])){
+                // validation Ok
+                die('success');
+            }else {
+                // load view with errors
+                $this->view('users/login', $data);
+            }
+
         } else{
             // init data
             $data = [

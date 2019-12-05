58a0a95933ecf12e10c506a225ff70dde5b7ac5f
kesylo <keming.loic@gmail.com>
2019-12-05 17:23:59 +0100

    Deployment ready

--- app/config/config.php
+++ app/config/config.php
@@ -1,16 +1,32 @@
 <?php
 
-// DB params
+// Deployment Settings
+
+/*
+define('DB_HOST', '192.168.0.123');
+define('DB_USER', 'root');
+define('DB_PASS', 'ZKZ7ajZxLMn3');
+define('DB_NAME', 'cityappdberp');
+
+// url root
+define('URLROOT', 'http://192.168.0.123/public');
+*/
+
+
+// Development Settings
+
 define('DB_HOST', 'localhost');
 define('DB_USER', 'userloic');
 define('DB_PASS', 'Ph03n1x.lo1c');
 define('DB_NAME', 'cityappdberp');
 
+// url root
+define('URLROOT', 'http://localhost/public');
+
+
+
 // App root constant
 define('APPROOT', dirname(dirname(__FILE__)));
-
-// url root
-define('URLROOT', 'http://localhost/CityCenter');
 
 // site name
 define('SITENAME', 'City Apartments');
